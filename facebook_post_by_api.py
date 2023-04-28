14ac6352f58dae4890d5a83286fc20fe625a7025
guram-asanidze-1-btu-edu-ge <131129552+guram-asanidze-1-btu-edu-ge@users.noreply.github.com>
2023-04-28 15:17:00 +0400

    Update facebook_post_by_api.py

--- facebook_post_by_api.py
+++ facebook_post_by_api.py
@@ -1,13 +1,15 @@
 import facebook as fb
 
 
-token='...q3xZB8b8nIlb3QoIstE04GR6HpgZAtk8QD1vf6glp4...'
+token='...q3xZB8b8nIlb3QoIstE04GR6HpgZAtk8QD1vf6glp4...' #use your permanent token
 asafb = fb.GraphAPI(token)
 id = '19537...' #your page ID where you are going to post
-pages_data = asafb.get_object('19537.../posts')
+pages_data = asafb.get_object('19537.../posts') #to get posts
 
 try:
     asafb.put_object(id, 'photos', message= '👀 something : ...' +'\n'+'' #interesting #facts' ,url=kart) #+'\n'+' this mean new line
+except Exception as e:
+    print(e)
 
 #example of post https://www.facebook.com/saintereso13/posts/pfbid02nTui77PAPcgFaXpvqmnmeTn596mcSDmjzL7SadMsUZAmSZZoF3NYvunN4ifJcedPl
 
